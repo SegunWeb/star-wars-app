9ecb49974c1f3f4623b53d36a0a09d6b692b04a8
SegunWeb <mrsegun2014@gmail.com>
2021-08-16 14:19:39 +0300

    Updates

---
+++ precache-manifest.825652a8d1d7fae21c7cb910887a4111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879c1520aa0532391bbda17bae1fdda4",
+    "url": "/star-wars-app/index.html"
+  },
+  {
+    "revision": "a18a7d2dbb0942bfd8fc",
+    "url": "/star-wars-app/static/css/main.a588030e.chunk.css"
+  },
+  {
+    "revision": "a1f064c902308a26b217",
+    "url": "/star-wars-app/static/js/2.01393a7b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/star-wars-app/static/js/2.01393a7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a18a7d2dbb0942bfd8fc",
+    "url": "/star-wars-app/static/js/main.53c6b056.chunk.js"
+  },
+  {
+    "revision": "01d7141fa44d89c53763",
+    "url": "/star-wars-app/static/js/runtime-main.a19b4f48.js"
+  }
+]);
